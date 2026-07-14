d8769e2be2ca3f6342bdda59adff8f2fd05b07fc
Dima Ejel <dimaejel79@gmail.com>
2026-07-13 18:06:59 -0700

    Scroll Animations

--- script.js
+++ script.js
@@ -2,8 +2,45 @@ const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
 
-menuBtn.addEventListener("click", () => {
+if(menuBtn){
 
-    navLinks.classList.toggle("show");  //iza mkhfiyi tbyn w iza mbyni tekhtafa
+    menuBtn.addEventListener("click", () => {
+
+        navLinks.classList.toggle("show");
+
+    });
+
+}
+
+
+// Scroll Animations
+
+ScrollReveal().reveal('.services', {
+
+    delay: 200,
+    distance: '50px',
+    duration: 1000,
+    origin: 'bottom'
+
+});
+
+
+ScrollReveal().reveal('.card', {
+
+    interval: 200,
+    delay: 300,
+    distance: '50px',
+    duration: 1000,
+    origin: 'bottom'
+
+});
+
+
+ScrollReveal().reveal('.technologies, .about, .contact', {
+
+    delay: 300,
+    distance: '60px',
+    duration: 1000,
+    origin: 'bottom'
 
 });
